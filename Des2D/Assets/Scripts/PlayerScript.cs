e11be1fcd90232212cea08e2c480e45b317676ce
Truong Quy <truongquybrvt96@gmail.com>
2017-05-20 23:49:07 +0700

    Xóa group.docx

---
+++ Des2D/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float speed = 10f;
+	private float horizon = 0f;
+	Rigidbody2D myRig;
+	// Use this for initialization
+	void Start () {
+		myRig = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		horizon = Input.GetAxisRaw ("Horizontal");
+		Vector3 pos = transform.position;
+		pos.x += horizon * speed * Time.deltaTime;
+		transform.position = pos;
+		//myRig.velocity = new Vector2 (speed, myRig.velocity.y);
+	}
+}
