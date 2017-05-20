c79c37d95626d30b3884c15596db0905797a2323
phithanh1230 <phiphi7430@gmail.com>
2017-05-21 01:37:56 +0700

    Update

---
+++ Des2D/Assets/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class GameController : MonoBehaviour {
+	public static GameController game;
+	public GameObject startText;
+	// Use this for initialization
+	void Awake()
+	{
+		if (game==null)
+			game = this;
+		else if(game!=this) {
+			Destroy (gameObject);
+		}
+	}
+
+	public void ClearStartText()
+	{
+		startText.SetActive (false);
+	}
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
