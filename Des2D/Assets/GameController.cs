2cb3e7a63fcfed1b0233aee9926de3572c94f359
phithanh1230 <phiphi7430@gmail.com>
2017-05-21 15:15:39 +0700

    Updated

--- Des2D/Assets/GameController.cs
+++ Des2D/Assets/GameController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 public class GameController : MonoBehaviour {
 	public static GameController game;
-	public GameObject startText;
+	public GameObject startText,pauseText;
+	public bool isStart=false;
+	private bool isPause = false;
 	// Use this for initialization
 	void Awake()
 	{
@@ -20,7 +22,19 @@ public class GameController : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			isStart = true;
+		}
+		if (Input.GetKeyDown (KeyCode.Escape) && isStart) {
+			isPause = !isPause;
+		}	
+		if (isPause) {
+			pauseText.SetActive (true);
+			Time.timeScale = 0;
+		} else {
+			Time.timeScale = 1;
+			pauseText.SetActive (false);
+		}
 	}
 
 }
--- Des2D/Assets/PlayerController.cs
+++ Des2D/Assets/PlayerController.cs
@@ -6,13 +6,12 @@ public class PlayerController : MonoBehaviour {
 	/*WARNING: when you change value variable in code, It won't change in the game that's why you need to change directly
 	in components ! PugPug*/
 	private Rigidbody2D r2;
-	bool isStart = false;
 	private Animator anim;
 	public float speedForce = 250f;
 	//----var fly------------
 	private bool isFly = false;
 	public float upForce = 275f;
-	private float timeFly = 1f, timeCnt = 0;//we must wait 1s for the next flight
+	private float timeFly = 1.3f, timeCnt = 0;//we must wait 1.3s for the next flight
 	// Use this for initialization
 	void Start () {
 		r2 = GetComponent<Rigidbody2D> ();
@@ -21,16 +20,12 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (!isStart) {
-			//we need to init for the first time and prepare to play the game
-			if (Input.GetKeyDown (KeyCode.Space))
-				isStart = true;
-		} else {
+		if (GameController.game.isStart) {
 			GameController.game.ClearStartText();//this function will clear the text in the middle of screen
 			anim.SetTrigger ("Run");
 			r2.AddForce (new Vector2 (speedForce, 0));//uses addforce to make velocity :v
 			//--------Check condition to fly-------------
-			if (!isFly && isStart) {
+			if (!isFly) {
 				if (Input.GetKeyDown (KeyCode.Space)) {
 					r2.AddForce (new Vector2 (0, upForce));
 					isFly = true;
